96166a9679b7c569ed1bfaa8121268b771af9431
jcarrascoa7 <jcarrascoa@uc.cl>
2022-03-30 23:34:12 -0300

    Character movement added (it doesn't change with the player head direction yet)

---
+++ Assets/Scripts/ContinuousMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Instanciamos UnityXR and UnityXRInteractionToolkit para poder obtener el input del touchpad
+
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ContinuousMovement : MonoBehaviour
+{
+
+    // Declaramos la variable del input que queremos usar. Se selecciona en Unity
+    public XRNode inputSource;
+
+    // Vector que tendrá la dirección del movimiento
+    private Vector2 inputAxis;
+
+    // Controlador del jugador.
+    private CharacterController character;
+
+    // Velocidad de movimiento
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Obtenemos el componente CharacterController del objeto
+        character = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Es una manera análoga al uso de InputDevices.GetDevicesWithCharacteristics
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+
+        // Ahora escuchamos el input del touchpad en cada frame
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
+    }
+
+    // Update is called once per frame. El movimiento lo gestionamos acá en lugar de en la función Update(). De esta forma el movimiento
+    // se actualizará cada vez que unity actualice las físicas del juego.
+    private void FixedUpdate()
+    {
+        Vector3 direction = new Vector3(inputAxis.x, 0, inputAxis.y);
+        character.Move(direction * Time.fixedDeltaTime * speed);
+    }
+}
