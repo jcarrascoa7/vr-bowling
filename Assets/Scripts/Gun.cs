90280c1492d6434e6ca556916af7aa33c87745bd
jcarrascoa7 <jcarrascoa@uc.cl>
2022-04-01 21:01:06 -0300

    Gun shots function and teleport deactivation while grabbing the gun added

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float speed = 40f;
+    public GameObject bullet;
+    public Transform barrel;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+    public void Fire()
+    {
+        GameObject spawnedBullet = Instantiate(bullet, barrel.position, barrel.rotation);
+        spawnedBullet.GetComponent<Rigidbody>().velocity = barrel.forward * speed;
+        audioSource.PlayOneShot(audioClip);
+        Destroy(spawnedBullet, 3);
+    }
+}
--- Assets/Scripts/LocomotionController.cs
+++ Assets/Scripts/LocomotionController.cs
@@ -10,10 +10,13 @@ public class LocomotionController : MonoBehaviour
     public InputHelpers.Button teleportActivationButton;
     public float activationThreshold = 0.1f;
 
+    // El {get; set;} es para que esta variable sea modificable mediante un evento custom (por ejemplo al tener la pistola en la mano)
+    public bool EnableRightTeleport {get; set; } = true;
+
     // Update is called once per frame
     void Update()
     {
-        if (rightTeleportRay)
+        if (rightTeleportRay && EnableRightTeleport)
         {
             rightTeleportRay.gameObject.SetActive(CheckIfActivated(rightTeleportRay));
         }
